1105ffa9b9ed4e036a84bf556e757da70e5c1119
Sérgio Koerich Pereira <129325112+sergiokoerich@users.noreply.github.com>
2023-05-14 21:01:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+
+const imagemBotaoTrocarDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro")
+
+    if(modoEscuroEstaAtivo) {
+        imagemBotaoTrocarDeTema.setAttribute("src", "./src/imagens/sun.png");
+    }else {
+        body.classList.add("modo-escuro");
+
+        imagemBotaoTrocarDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+
+});
